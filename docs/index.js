ccf4bb5b7bac20ec7800b0f0b5ccf91b24bcc364
米須諒 <r.komesu@churadata.okinawa>
2022-05-29 17:36:44 +0900

    bugfix

--- docs/index.js
+++ docs/index.js
@@ -2,5 +2,5 @@ console.log('index.js: loaded')
 
 import Modal from './lib/modal.js'
 
-const modal_1 = new Modal('easyModal', 'modalClose', 'overlay')
-modal_1.open()
+// const modal_1 = new Modal('easyModal', 'modalClose', 'overlay')
+// modal_1.open()
