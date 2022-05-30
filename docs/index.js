10c3670763759d88b3156ea4f53ac563180e3825
米須諒 <r.komesu@churadata.okinawa>
2022-05-30 22:50:50 +0900

    bugfix

--- docs/index.js
+++ docs/index.js
@@ -2,5 +2,5 @@ console.log('index.js: loaded')
 
 import Modal from './lib/modal.js'
 
-const modal_1 = new Modal('easyModal', 'modalClose', 'overlay')
+const modal_1 = new Modal('easyModal')
 modal_1.open()
--- docs/lib/modal.js
+++ docs/lib/modal.js
@@ -1,14 +1,14 @@
 export default class Modal {
-  constructor(modalID, overlayClass, closeButtonClass) {
+  constructor(modalID) {
     this.modal = document.getElementById(modalID)
-    this.overlay = this.modal.getElementsByClassName(overlayClass)[0]
-    this.closeButton = this.modal.getElementsByClassName(closeButtonClass)[0]
+    this.closeButton = this.modal.getElementsByClassName('modalClose')[0]
     this.init()
   }
   init() {
-    this.modal.addEventListener('click', this.open)
-    this.overlay.addEventListener('click', this.close)
-    this.closeButton.addEventListener('click', this.close)
+    this.modal.addEventListener('click', (e) => {
+      if (e.target == this.modal) this.close()
+    })
+    this.closeButton.addEventListener('click', this.close.bind(this))
   }
   open() {
     this.modal.style.display = 'block'
