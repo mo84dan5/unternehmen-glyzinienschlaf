726e245a5f4d42d7feb3223021a5c531937048c1
米須諒 <r.komesu@churadata.okinawa>
2022-06-01 00:01:30 +0900

    bugfix

--- docs/index.js
+++ docs/index.js
@@ -3,17 +3,24 @@ console.log('index.js: loaded')
 import Modal from './lib/modal.js'
 import SubmitModal from './lib/submit-modal.js'
 
-const init_modal = new SubmitModal('initModal')
-init_modal.setButtonFunc(async () => {
-  if (
-    typeof DeviceMotionEvent !== 'undefined' &&
-    typeof DeviceMotionEvent.requestPermission === 'function'
-  ) {
-    await DeviceOrientationEvent.requestPermission()
-  }
-})
-init_modal.open()
-
+async function requestPermission() {
+  const init_modal = new SubmitModal('initModal')
+  init_modal.setButtonFunc(async () => {
+    if (
+      typeof DeviceMotionEvent !== 'undefined' &&
+      typeof DeviceMotionEvent.requestPermission === 'function'
+    ) {
+      await DeviceOrientationEvent.requestPermission()
+      resolve()
+    }
+  })
+  init_modal.open()
+}
+async function main() {
+  await requestPermission()
+  const scene = new THREE.Scene()
+}
+main()
 // const modal_1 = new Modal('easyModal', () => {
 //   console.log('aaa')
 // })
